17d6d5fcddc678d441f6f1e888265e290c041965
kinggggg <liweibooooo@126.com>
2018-03-30 14:43:54 +0800

    使用第三方依赖的方法

--- src/main/java/com/zeek/todo/AppMain.java
+++ src/main/java/com/zeek/todo/AppMain.java
@@ -1,7 +1,11 @@
 package com.zeek.todo;
 
+import org.apache.commons.lang3.CharUtils;
+
 public class AppMain{
 	public static void main(String[] args) {
 		System.out.println("Hello Gradle!");
+
+		System.out.println(CharUtils.toChar("Hello World!"));
 	}
 }
